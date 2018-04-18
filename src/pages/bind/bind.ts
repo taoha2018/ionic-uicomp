eec5131ed7468d3698b89a1f4d7cb92247de3413
taoha2018 <hitonym@naver.com>
2018-04-18 20:36:46 +0900

    modal & slide

--- src/pages/bind/bind.ts
+++ src/pages/bind/bind.ts
@@ -7,8 +7,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
-
-@IonicPage({name:"bindPage"})
+//{name:'bindPage'}
+@IonicPage()
 @Component({
   selector: 'page-bind',
   templateUrl: 'bind.html',
@@ -18,12 +18,20 @@ export class BindPage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
+  // 화면이 load되고 난후 에 바로 호출되는 life cycle method 메서드
   ionViewDidLoad() {
-    alert('ionViewDidLoad BindPage');
+    console.log('1. >> BindPage ionViewDidLoad  ' + this.navParams.get("name"));
   }
 
-  goBack(){
+  goBack() {
     this.navCtrl.pop();
   }
 
+  ionViewWillEnter() {
+    console.log("2.>> BindPage ionViewWillEnter() 호출");
+  }
+
+  ionViewWillUnload() {
+    console.log("3.>> BindPage ionViewWillUnload() 호출");
+  }
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,32 +1,54 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {BindPage} from "../bind/bind";
+import {ComponentPage} from "../component/component";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
   items = [
-    {"id":1, "name":"첫번째 item"},
-    {"id":2, "name":"두번째 item"},
-    {"id":3, "name":"세번째 item"},
+    {"id":1,"name":"Component"},
+    {"id":2,"name":"두번째 Item"},
+    {"id":3,"name":"세번째 Item"},
   ];
 
-  disabledSwitch = false;
-  username:any;
+  disabledSwitch=false;
+
+  userName:any;
 
   constructor(public navCtrl: NavController) {
-    this.username = "아이오닉";
+    //this.userName = "아이오닉";
   }
 
-  itemSelected(item){
-    alert(item.id);
-  }
-  clickButton(event) {
-    console.log(event.clientX+ ", " + event.clientY);
-    this.navCtrl.push("bindPage");
+  //event handler 메서드
+  itemSelected(item) {
+    if(item.id === 1) {
+      this.navCtrl.push("ComponentPage");
+    }
   }
 
+  myButton(event) {
+    if(typeof this.userName !== "undefined"){
+      console.log(event.target.textContent + " " + event.clientX + "," +
+      event.clientY);
+      this.navCtrl.push("BindPage",{name:this.userName});
+    }else {
+      //입력값이 없는 경우
+      alert("userName 입력하세요!");
+    }
+  }
 
+  ionViewDidLoad() {
+    console.log("1.HomePage ionViewDidLoad() 호출");
+  }
+
+  ionViewWillEnter() {
+    console.log("2.HomePage ionViewWillEnter() 호출");
+  }
+
+  ionViewWillUnload() {
+    console.log("3.HomePage ionViewWillUnload() 호출");
+  }
 }
